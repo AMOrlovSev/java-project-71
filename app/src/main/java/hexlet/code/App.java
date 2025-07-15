b5d0dcca8c2122fcbbcbd9be9538b8a383b04a06
Artem Orlov <amorlov.sev@gmail.com>
2025-07-15 16:02:58 +0300

    Add picocli to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
