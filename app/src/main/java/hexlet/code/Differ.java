b4a63108a746b7c6b99201888a92198a7cb796c2
Artem Orlov <amorlov.sev@gmail.com>
2025-07-16 20:20:24 +0300

    Edit Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,32 +1,57 @@
 package hexlet.code;
 
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Differ {
-    public static String generate(Map<String, Object> map1, Map<String, Object> map2) {
-        String result = "{\n";
+    private static final String INDENT = "  ";
+    private static final String ADDED_PREFIX = "+ ";
+    private static final String REMOVED_PREFIX = "- ";
+    private static final String UNCHANGED_PREFIX = "  ";
 
-        Set<String> uniqueKeys = new HashSet<String>();
+
+    public static String generate(Map<String, Object> map1, Map<String, Object> map2) {
+        StringBuilder result = new StringBuilder("{\n");
+
+        Set<String> uniqueKeys = new TreeSet<>();
         uniqueKeys.addAll(map1.keySet());
         uniqueKeys.addAll(map2.keySet());
-        var sortedKeys = uniqueKeys.stream().sorted().toList();
 
-        for (var key : sortedKeys) {
-            if (map1.getOrDefault(key, String.valueOf("")).equals(map2.getOrDefault(key, String.valueOf("")))) {
-                result += "    " + key + ": " + map1.get(key) + "\n";
+        for (String key : uniqueKeys) {
+            Object value1 = map1.get(key);
+            Object value2 = map2.get(key);
+
+            if (equals(value1, value2)) {
+                appendLine(result, UNCHANGED_PREFIX, key, value1);
                 continue;
             }
+
             if (map1.containsKey(key)) {
-                result += "  - " + key + ": " + map1.get(key) + "\n";
+                appendLine(result, REMOVED_PREFIX, key, value1);
             }
             if (map2.containsKey(key)) {
-                result += "  + " + key + ": " + map2.get(key) + "\n";
+                appendLine(result, ADDED_PREFIX, key, value2);
             }
         }
 
-        return result + "}";
+        return result.append("}").toString();
+    }
+
+    private static void appendLine(StringBuilder builder, String prefix, String key, Object value) {
+        builder.append(INDENT)
+                .append(prefix)
+                .append(key)
+                .append(": ")
+                .append(value)
+                .append("\n");
+    }
+
+    private static boolean equals(Object o1, Object o2) {
+        if (o1 == null) {
+            return o2 == null;
+        }
+        return o1.equals(o2);
     }
 }
 
