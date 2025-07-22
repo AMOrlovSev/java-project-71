ce58184fd375ba431cb883aadd9b4297cc171df2
Artem Orlov <amorlov.sev@gmail.com>
2025-07-22 17:38:41 +0300

    edit DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,51 +1,75 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
+    private static String resultStylishJson;
+    private static String resultStylishYaml;
+    private static String resultPlainJson;
+    private static String resultPlainYaml;
+    private static String resultJsonJson;
+    private static String resultJsonYaml;
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        resultStylishJson = Files.readString(Parser.resolvePath("resultStylish.json"));
+        resultStylishYaml = Files.readString(Parser.resolvePath("resultStylish.yaml"));
+        resultPlainJson = Files.readString(Parser.resolvePath("resultPlain.json"));
+        resultPlainYaml = Files.readString(Parser.resolvePath("resultPlain.yaml"));
+        resultJsonJson = Files.readString(Parser.resolvePath("resultJson.json"));
+        resultJsonYaml = Files.readString(Parser.resolvePath("resultJson.yaml"));
+    }
+
+    @Test
+    public void testGenerateJsonDefault() throws Exception {
+        String result = Differ.generate("file1.json", "file2.json");
+        assertEquals(resultStylishJson, result);
+    }
+
+    @Test
+    public void testGenerateYamlDefault() throws Exception {
+        String result = Differ.generate("file1.yaml", "file2.yaml");
+        assertEquals(resultStylishYaml, result);
+    }
+
     @Test
     public void testGenerateJsonStylish() throws Exception {
         String result = Differ.generate("file1.json", "file2.json", "stylish");
-        String correct = Files.readString(Parser.resolvePath("resultStylish.json"));
-        assertEquals(result, correct);
+        assertEquals(resultStylishJson, result);
     }
 
     @Test
     public void testGenerateYamlStylish() throws Exception {
         String result = Differ.generate("file1.yaml", "file2.yaml", "stylish");
-        String correct = Files.readString(Parser.resolvePath("resultStylish.yaml"));
-        assertEquals(result, correct);
+        assertEquals(resultStylishYaml, result);
     }
 
     @Test
     public void testGenerateJsonPlain() throws Exception {
         String result = Differ.generate("file1.json", "file2.json", "plain");
-        String correct = Files.readString(Parser.resolvePath("resultPlain.json"));
-        assertEquals(result, correct);
+        assertEquals(resultPlainJson, result);
     }
 
     @Test
     public void testGenerateYamlPlain() throws Exception {
         String result = Differ.generate("file1.yaml", "file2.yaml", "plain");
-        String correct = Files.readString(Parser.resolvePath("resultPlain.yaml"));
-        assertEquals(result, correct);
+        assertEquals(resultPlainYaml, result);
     }
 
     @Test
     public void testGenerateJsonJson() throws Exception {
         String result = Differ.generate("file1.json", "file2.json", "json");
-        String correct = Files.readString(Parser.resolvePath("resultJson.json"));
-        assertEquals(result, correct);
+        assertEquals(resultJsonJson, result);
     }
 
     @Test
     public void testGenerateYamlJson() throws Exception {
         String result = Differ.generate("file1.yaml", "file2.yaml", "json");
-        String correct = Files.readString(Parser.resolvePath("resultJson.yaml"));
-        assertEquals(result, correct);
+        assertEquals(resultJsonYaml, result);
     }
 
 }
